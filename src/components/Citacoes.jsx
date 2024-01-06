b6bd9b65d10d1bd627614ee34b312676c5023772
Eder Henrique <ederhscc@gmail.com>
2024-01-06 13:05:16 -0300

    added tranlate text function

--- src/components/Citacoes.jsx
+++ src/components/Citacoes.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Citacoes = ({ texto, autor }) => {
+  const [traducao, setTraducao] = useState();
+
+  async function traduzirCitacao(idioma) {
+    try {
+      const resposta = await fetch("https://libretranslate.de/translate", {
+        method: "POST",
+        body: JSON.stringify({
+          q: texto,
+          source: "pt",
+          target: idioma,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await resposta.json();
+
+      setTraducao(data.translatedText);
+    } catch (error) {
+      console.log("Erro ao traduzir: ", error);
+    }
+  }
+
+  // Para trocar o texto
+  useEffect(() => {
+    setTraducao("");
+  }, [texto]);
+
   return (
     <div>
       <blockquote className="blockquote">
-        <p>{texto}</p>
+        <p>{traducao ? traducao : texto}</p>
         <footer className="blockquote-footer">{autor}</footer>
       </blockquote>
+      <button
+        className="btn btn-primary mr-1"
+        onClick={() => traduzirCitacao("en")}
+      >
+        Traduzir para o inglês
+      </button>
+      <button
+        className="btn btn-secondary m-1"
+        onClick={() => traduzirCitacao("es")}
+      >
+        Traduzir para o espanhol
+      </button>
     </div>
   );
 };
